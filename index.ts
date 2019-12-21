a7a36c2b114681ed22acff9bdb28d70ed1d2ff86
CKOwen <44505297+OwenCK@users.noreply.github.com>
2019-12-21 18:44:19 +0000

    index file

--- index.ts
+++ index.ts
@@ -1,17 +1,37 @@
-// import SlackBot from "./lib/slackBot";
-// import { SpotifyQueue } from "./lib/spotifyQueue";
+import { Config } from "./lib/Config";
+import { CommandHandler } from "./lib/CommandHandler";
+import { Controller } from "./lib/Controller";
+import { Spotify } from "./lib/Spotify";
+import { Queue } from "./lib/Queue";
+import { DeviceSelector } from "./lib/DeviceSelector";
+import { Votes } from "./lib/Votes";
+import { NowPlaying } from "./lib/NowPlaying";
+import { SearchHandler } from "./lib/SearchHandler";
+import { Slack } from "./lib/Slack";
+import { Bot } from "./lib/Bot";
 
-// const queue = new SpotifyQueue();
-// const bot = new SlackBot(queue);
+const config = new Config();
+const spotify = new Spotify(config);
+const queue = new Queue(config, spotify);
+const controller = new Controller(config, queue);
+const searchHandler = new SearchHandler(config, spotify, queue);
+const deviceSelector = new DeviceSelector(config, spotify);
+const votes = new Votes(config, queue);
+const nowPlaying = new NowPlaying(queue);
+const commandHandler = new CommandHandler(controller, searchHandler, votes, deviceSelector, nowPlaying);
+const slack = new Slack(config);
+const bot = new Bot(config, slack, commandHandler);
 
-// // bot.listenForMessages();
+async function start() {
+    await spotify.authorize();
+    await bot.listen();
+}
 
-// queue
-//     .authorize()
-//     .then(function() {
-//         console.log("Spotify authorized, listening for slack messages");
-//         bot.listenForMessages();
-//     })
-//     .catch(function() {
-//         console.error("Failed to authorize Spotify");
-//     });
+console.log("Authorizing spotify");
+start()
+    .then(() => {
+        console.log("Spotify and slack authenticated");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
